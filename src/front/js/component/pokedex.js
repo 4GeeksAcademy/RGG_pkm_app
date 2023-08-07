8878a325e631a5d064275d6203d0804a448b687e
Ruben Garcia <64198290+rubengargu@users.noreply.github.com>
2023-08-07 18:34:47 +0000

    call to api ok

--- src/front/js/component/pokedex.js
+++ src/front/js/component/pokedex.js
@@ -6,161 +6,58 @@ import  "../../styles/pokedex.css"
 
 
 export const Pokedex=()=>{
-//     const [result, setResult] = React.useState([]);
-//     const [poke, setPoke] = React.useState([]);
-//     const [load, setLoad] = React.useState('true');
-//     const arr = [];
-//     useEffect(() => {
-//      fetch('https://pokeapi.co/api/v2/pokemon/?limit=50')
-//      .then((response) => response.json())
-//      .then((data) => setResult(
-//      data.results.map((item) => {
-//      fetch(item.url)
-//      .then((response) => response.json())
-//      .then((allpokemon) => arr.push(allpokemon));
-//      setPoke(arr);
-//    }),
-//    ));
-//    }, []);
-//    setTimeout(() => {
-//    setLoad(false);
-//    }, 1000);
- 
-   return(
-   <div>
-     <div className="pokedex-bg">
-            <ul className="list-types">
-               
-                <li className="nav-item"><button className="btn btn-header" id="ver-todos">Ver todos</button></li>
-                <li className="nav-item"><button className="btn btn-header normal" id="normal">Normal</button></li>
-                <li className="nav-item"><button className="btn btn-header fire" id="fire">Fire</button></li>
-                <li className="nav-item"><button className="btn btn-header water" id="water">Water</button></li>
-                <li className="nav-item"><button className="btn btn-header grass" id="grass">Grass</button></li>
-                <li className="nav-item"><button className="btn btn-header electric" id="electric">Electric</button></li>
-                <li className="nav-item"><button className="btn btn-header ice" id="ice">Ice</button></li>
-                <li className="nav-item"><button className="btn btn-header fighting" id="fighting">Fighting</button></li>
-                <li className="nav-item"><button className="btn btn-header poison" id="poison">Poison</button></li>
-                <li className="nav-item"><button className="btn btn-header ground" id="ground">Ground</button></li>
-                <li className="nav-item"><button className="btn btn-header flying" id="flying">Flying</button></li>
-                <li className="nav-item"><button className="btn btn-header physic" id="physic">Physic</button></li>
-                <li className="nav-item"><button className="btn btn-header bug" id="bug">Bug</button></li>
-                <li className="nav-item"><button className="btn btn-header rock" id="rock">Rock</button></li>
-                <li className="nav-item"><button className="btn btn-header ghost" id="ghost">Ghost</button></li>
-                <li className="nav-item"><button className="btn btn-header dark" id="dark">Dark</button></li>
-                <li className="nav-item"><button className="btn btn-header dragon" id="dragon">Dragon</button></li>
-                <li className="nav-item"><button className="btn btn-header steel" id="steel">Steel</button></li>
-                <li className="nav-item"><button className="btn btn-header fairy" id="fairy">Fairy</button></li>
-
-            </ul>
-       <div className="todos">
-        <div className="pokemon-todos" id="listaPokemon">
-            <div className="pokemon">
-                <p className="pokemon-id-back"></p>
-                <div className="pokemon-imagen"><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png"
-                 alt="Pikachu"></img>
-                 <div className="pokemon-info">
-                    <div className="nombre-contenedor">
-                        <p className="pokemon-id">#025</p>
-                        <h2 className="pokemon-nombre">Pikachu</h2>
-                    </div>
-                    <div className="d-flex f-column columna">
-                    <div className="pokemon-tipos">
-                        <p className="electric tipo">Electric</p>
-                        <p className="fighting tipo">Fighting</p>
-                        </div>
-                        <div className="pokemon-stats">
-                            <p className="stat">4m</p>
-                            <p className="stat">60kg</p>
-                        </div>
-                        </div>
-
-                 </div>
-            </div>
-      
-
-       </div>
-       <div className="pokemon">
-                <p className="pokemon-id-back">#025</p>
-                <div className="pokemon-imagen"><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png"
-                 alt="Pikachu"></img>
-                 <div className="pokemon-info">
-                    <div className="nombre-contenedor">
-                        <p className="pokemon-id">#025</p>
-                        <h2 className="pokemon-nombre">Pikachu</h2>
-                    </div>
-                    <div className="d-flex f-column columna">
-                    <div className="pokemon-tipos">
-                        
-                        <p className="electric tipo">Electric</p>
-                        <p className="fighting tipo">Fighting</p>
-                        </div>
-                        <div className="pokemon-stats">
-                            <p className="stat">4m</p>
-                            <p className="stat">60kg</p>
-                        </div>
-                        </div>
-                        
-
-               
-            </div>
-        </div>
-
-       </div>
-       <div className="pokemon">
-                <p className="pokemon-id-back">#025</p>
-                <div className="pokemon-imagen"><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png"
-                 alt="Pikachu"></img>
-                 <div className="pokemon-info">
-                    <div className="nombre-contenedor">
-                        <p className="pokemon-id">#025</p>
-                        <h2 className="pokemon-nombre">Pikachu</h2>
-                    </div>
-                    <div className="d-flex f-column columna">
-                    <div className="pokemon-tipos">
-                        <p className="electric tipo">Electric</p>
-                        <p className="fighting tipo">Fighting</p>
-                        </div>
-                        <div className="pokemon-stats">
-                            <p className="stat">4m</p>
-                            <p className="stat">60kg</p>
-                        </div>
-                        </div>
-
-                 </div>
-            </div>
-      
-
-       </div>
-       <div className="pokemon">
-                <p className="pokemon-id-back">#025</p>
-                <div className="pokemon-imagen"><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png"
-                 alt="Pikachu"></img>
-                 <div className="pokemon-info">
-                    <div className="nombre-contenedor">
-                        <p className="pokemon-id">#025</p>
-                        <h2 className="pokemon-nombre">Pikachu</h2>
-                    </div>
-                    <div className="d-flex f-column columna">
-                    <div className="pokemon-tipos">
-                        
-                        <p className="electric tipo">Electric</p>
-                        <p className="fighting tipo">Fighting</p>
-                        </div>
-                        <div className="pokemon-stats">
-                            <p className="stat">4m</p>
-                            <p className="stat">60kg</p>
-                        </div>
-                        </div>
-                        
-
-               
-            </div>
-        </div>
-
-       </div>
-       </div></div>
-       </div>
-
-</div>
-    )
-}
+    const [result, setResult] = React.useState([]);
+    const [poke, setPoke] = React.useState([]);
+    const [load, setLoad] = React.useState('true');
+  
+    const arr = [];
+    
+    useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon/?limit=151')
+        .then((response) => response.json())
+        .then((data) => setResult(
+          data.results.map((item) => {
+            fetch(item.url)
+              .then((response) => response.json())
+              .then((allpokemon) => arr.push(allpokemon));
+              setPoke(arr);
+          }),
+        ));
+    }, []);
+  
+     setTimeout(() => {
+       setLoad(false);
+       console.log(poke)
+     }, 1000);
+   
+    return (
+      <div className="App">
+         <div className='pokegallery'>
+  
+          { load ? (
+            <p>Loading...</p>
+          ) : (
+  
+            poke.map((img, i) => (
+              <div id={img.id} key={img.id}>
+  
+                <div className='card' style={{ width: '10rem', height: '15rem', backgroundColor: '#F0F0C9' }}>
+                  <img  src={img.sprites.front_default} alt='pokemon' />
+                  <div >
+                    <h5 >{img.name}</h5>
+                    <h6>type: {img.types[0].type.name}</h6>
+                  </div>
+                </div>
+  
+  
+              </div>
+            ))
+          )}
+  
+  
+  
+  </div>
+      </div>
+    );
+  }
+  
