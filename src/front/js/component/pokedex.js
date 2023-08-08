538d7bcbaddfa90c615fcfc079fc819a043e8951
Ruben Garcia <64198290+rubengargu@users.noreply.github.com>
2023-08-08 10:47:14 +0000

    buscador ok

--- src/front/js/component/pokedex.js
+++ src/front/js/component/pokedex.js
@@ -1,63 +1,98 @@
-import React,{useState,useEffect, useSyncExternalStore, useContext} from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
-import  "../../styles/demo.css";
-import  "../../styles/pokedex.css"
+import "../../styles/demo.css";
+import "../../styles/pokedex.css";
 
+export const Pokedex = () => {
+  const [result, setResult] = useState([]);
+  const [poke, setPoke] = useState([]);
+  const [load, setLoad] = useState(true);
+  const [buscar, setBuscar] = useState([]);
+  const [tablaPokemon, setTablaPokemon] = useState([]);
+  const [busqueda, setBusqueda] = useState("");
 
+  const handleChange = (e) => {
+    setBusqueda(e.target.value);
+    filtrar(e.target.value);
+  };
 
-export const Pokedex=()=>{
-    const [result, setResult] = React.useState([]);
-    const [poke, setPoke] = React.useState([]);
-    const [load, setLoad] = React.useState('true');
-  
-    const arr = [];
-    
-    useEffect(() => {
+  const filtrar = (terminoBusqueda) => {
+    let resultadoBusqueda = tablaPokemon.filter((elemento) =>
+      elemento.name.toLowerCase().includes(terminoBusqueda.toLowerCase())
+    );
+    setBuscar(resultadoBusqueda);
+  };
+
+  useEffect(() => {
     fetch('https://pokeapi.co/api/v2/pokemon/?limit=151')
-        .then((response) => response.json())
-        .then((data) => setResult(
-          data.results.map((item) => {
-            fetch(item.url)
-              .then((response) => response.json())
-              .then((allpokemon) => arr.push(allpokemon));
-              setPoke(arr);
-          }),
-        ));
-    }, []);
-  
-     setTimeout(() => {
-       setLoad(false);
-       console.log(poke)
-     }, 1000);
-   
-    return (
-      <div className="App">
-         <div className='pokegallery'>
-  
-          { load ? (
-            <p>Loading...</p>
-          ) : (
-  
-            poke.map((img, i) => (
+      .then((response) => response.json())
+      .then((data) => {
+        const fetchPromises = data.results.map((item) =>
+          fetch(item.url).then((response) => response.json())
+        );
+
+        Promise.all(fetchPromises)
+          .then((allpokemon) => {
+            setPoke(allpokemon);
+            setTablaPokemon(allpokemon); // Guarda los datos en tablaPokemon para el filtrado
+            setLoad(false);
+          })
+          .catch((error) => {
+            console.error('Error fetching Pokémon data:', error);
+            setLoad(false); // Set load to false even in case of an error
+          });
+      })
+      .catch((error) => {
+        console.error('Error fetching Pokémon list:', error);
+        setLoad(false); // Set load to false in case of an error
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <div className='pokegallery'>
+        <input
+          className="form-control inputBuscar"
+          value={busqueda}
+          placeholder="¿Qué Pokémon buscas?"
+          onChange={handleChange}
+        />
+        <button className="btn btn-success">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+          </svg>
+        </button>
+
+        {load ? (
+          <p>Loading...</p>
+        ) : (
+          buscar.length > 0 ? ( // Utiliza 'buscar' para el filtrado
+            buscar.map((img, i) => (
               <div id={img.id} key={img.id}>
-  
                 <div className='card' style={{ width: '10rem', height: '15rem', backgroundColor: '#F0F0C9' }}>
-                  <img  src={img.sprites.front_default} alt='pokemon' />
-                  <div >
-                    <h5 >{img.name}</h5>
+                  <img src={img.sprites.front_default} alt='pokemon' />
+                  <div>
+                    <h5>{img.name}</h5>
                     <h6>type: {img.types[0].type.name}</h6>
                   </div>
                 </div>
-  
-  
               </div>
             ))
-          )}
-  
-  
-  
-  </div>
+          ) : (
+            poke.map((img, i) => (
+              <div id={img.id} key={img.id}>
+                <div className='card' style={{ width: '10rem', height: '15rem', backgroundColor: '#F0F0C9' }}>
+                  <img src={img.sprites.front_default} alt='pokemon' />
+                  <div>
+                    <h5>{img.name}</h5>
+                    <h6>type: {img.types[0].type.name}</h6>
+                  </div>
+                </div>
+              </div>
+            ))
+          )
+        )}
       </div>
-    );
-  }
-  
+    </div>
+  );
+};
