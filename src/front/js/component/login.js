15600094d123ff21ccc459e71dfb9456f420e065
Ruben Garcia <64198290+rubengargu@users.noreply.github.com>
2023-08-18 17:37:14 +0000

    login style almost ok

--- src/front/js/component/login.js
+++ src/front/js/component/login.js
@@ -6,24 +6,26 @@ import  "../../styles/login.css"
 
 export const Login = () =>{
     const [nombre, setNombre] = useState("")
-    const [pass, setPass] = useState("")
+    const [password, setPass] = useState("")
     const [alias, setAlias] = useState("")
     const [mail, setMail] = useState("")
+    const [confirmPass, setConfirmPass] = useState("")
 
 
     return(
         <div>
                <h2 className="title-login">Crea tu usuario en nuestra Pokedex!</h2>
             <form className="formulario">
-                <label for="name">Nombre:</label>
-                <input type="text" id="name" value={nombre} onChange={e => setNombre(e.target.value)} />
-                <label for="alias">Nickname:</label>
-                <input type="text" id="alias" value={alias} onChange={e => setAlias(e.target.value)} />
-                <label for="mail">Correo electrónico:</label>
-                <input type="mail" id="mail" value={mail} onChange={e => setMail(e.target.value)} />
-                <label for="password">Password</label>
-                <input type="password" id="password" value={pass} onChange={e => setPass(e.target.value)}/>
-                <button>Iniciar sesion</button>
+                {/* <label for="name">Nombre:</label> */}
+                <input className="input-login" type="text" id="name" placeholder="Nombre" value={nombre} onChange={e => setNombre(e.target.value)} />
+                {/* <label for="alias">Nickname:</label> */}
+                <input className="input-login" placeholder="Nickname" type="text" id="alias" value={alias} onChange={e => setAlias(e.target.value)} />
+                {/* <label for="mail">Correo electrónico:</label> */}
+                <input className="input-login" type="mail" placeholder="Correo electrónico" id="mail" value={mail} onChange={e => setMail(e.target.value)} />
+                {/* <label for="password">Password</label> */}
+                <input className="input-login" type="password" placeholder="Password" id="pass" value={password} onChange={e => setPass(e.target.value)}/>
+                <input className="input-login" type="password" placeholder="Confirm password" id="password" value={confirmPass} onChange={e => setConfirmPass(e.target.value)}/>
+                <button className="btn-danger">Crea tu usuario!</button>
             </form>
         </div>
     )
