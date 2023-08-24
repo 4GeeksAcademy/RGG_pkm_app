1fb36c772296ff2d82cde94eed757d0f770df671
Ruben Garcia <64198290+rubengargu@users.noreply.github.com>
2023-08-24 13:00:06 +0000

    begin with favs

--- src/front/js/component/pokedex.js
+++ src/front/js/component/pokedex.js
@@ -2,6 +2,27 @@ import React, { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import "../../styles/pokedex.css";
 
+function ClickCounter({ pokemonName }) {
+  const [count, setCount] = useState(0);
+
+  const addFav = () => {
+    alert("Has agregado " + pokemonName + " a mis favoritos");
+    
+  };
+
+  return (
+    <div>
+      <button onClick={addFav} className="button-favourites">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+          <path d="..." />
+        </svg>
+        Agregar a mis favoritos
+      </button>
+     
+    </div>
+  );
+}
+
 export const Pokedex = () => {
   const [result, setResult] = useState([]);
   const [poke, setPoke] = useState([]);
@@ -50,78 +71,65 @@ export const Pokedex = () => {
   return (
     <div className="App">
       <div className='pokegallery'>
-        <input className="form-control inputBuscar" value={busqueda} placeholder="¿Qué Pokémon buscas?"
-          onChange={handleChange}/>
+        <input className="form-control inputBuscar" value={busqueda} placeholder="¿Qué Pokémon buscas?" onChange={handleChange}/>
       
-
         {load ? (
           <p>Loading...</p>
         ) : (
           buscar.length > 0 ? ( 
             buscar.map((img, i) => (
-              <div className="row columna ">
-              <div className="card-pkm">
-              <div className="center-that"  id={img.id} key={img.id}>
-                
-                  <img src={img.sprites.front_default} alt='pokemon' />
-                  <div className='card'>
-                    <div className="nameNumber">
-                  <h5 className="pokemon-id">#{img.id}</h5>
-                    <h5 className="pokemon-name">{img.name}</h5>
+              <div className="row columna " key={img.id}>
+                <div className="card-pkm">
+                  <div className="center-that" id={img.id}>
+                    <img src={img.sprites.front_default} alt='pokemon' />
+                    <div className='card'>
+                      <div className="nameNumber">
+                        <h5 className="pokemon-id">#{img.id}</h5>
+                        <h5 className="pokemon-name">{img.name}</h5>
+                      </div>
+                      <div className="types">
+                        <h6></h6>
+                        <div className="type-pokedex">
+                          {img.types.map((type, index) => (
+                            <div key={index} className="type-box">
+                              {type.type.name}
+                            </div>
+                          ))}
+                        </div>
+                      </div>
+                      <Link to={`/pokedex/${img.id}`} className="detalle-pokedex">Detalle del pokemon</Link>
+                      <ClickCounter pokemonName={img.name} />
                     </div>
-                    <div className="types">
-        <h6></h6>
-        <div className="type-pokedex">
-        {img.types.map((type, index) => (
-            <div key={index} className="type-box">
-                {type.type.name}
-            </div>
-        ))}
-    </div>
-    </div>
-                    <Link to={`/pokedex/${img.id}`} className="detalle-pokedex">Detalle del pokemon</Link>
-                    <button className="btn-primary" onclick="#">Agregar a mis favoritos</button>
-
-
-
-                  
                   </div>
                 </div>
-              </div>
-             
               </div>
             ))
           ) : (
             poke.map((img, i) => (
-              <div className="row columna ">
-              <div className="card-pkm">
-              <div className="center-that"  id={img.id} key={img.id}>
-                
-                  <img src={img.sprites.front_default} alt='pokemon' />
-                  <div className='card'>
-                    <div className="nameNumber">
-                  <h5 className="pokemon-id">#{img.id}</h5>
-                    <h5 className="pokemon-name">{img.name}</h5>
+              <div className="row columna " key={img.id}>
+                <div className="card-pkm">
+                  <div className="center-that" id={img.id}>
+                    <img src={img.sprites.front_default} alt='pokemon' />
+                    <div className='card'>
+                      <div className="nameNumber">
+                        <h5 className="pokemon-id">#{img.id}</h5>
+                        <h5 className="pokemon-name">{img.name}</h5>
+                      </div>
+                      <div className="types">
+                        <h6></h6>
+                        <div className="type-pokedex">
+                          {img.types.map((type, index) => (
+                            <div key={index} className="type-box">
+                              {type.type.name}
+                            </div>
+                          ))}
+                        </div>
+                      </div>
+                      <Link to={`/pokedex/${img.id}`} className="detalle-pokedex">Detalle del pokemon</Link>
+                      <ClickCounter pokemonName={img.name} />
                     </div>
-                    <div className="types">
-        <h6></h6>
-        <div className="type-pokedex">
-        {img.types.map((type, index) => (
-            <div key={index} className="type-box">
-                {type.type.name}
-            </div>
-        ))}
-    </div>
-    </div>
-                    <Link to={`/pokedex/${img.id}`} className="detalle-pokedex">Detalle del pokemon</Link>
-
-
-
-                  
                   </div>
                 </div>
-              </div>
-             
               </div>
             ))
           )
