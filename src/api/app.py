de4f2f7f81221b99917fb461c69655b54d5a0b73
Ruben Garcia <64198290+rubengargu@users.noreply.github.com>
2023-09-19 18:29:49 +0000

    why rigo

--- src/api/app.py
+++ src/api/app.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 import bcrypt
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 
 app = Flask(__name__)
 CORS(app)
@@ -27,22 +28,38 @@ def register():
 # Endpoint de inicio de sesión
 @app.route('/login', methods=['POST'])
 def login():
-    data = request.json
-    user = users_collection.find_one({'username': data['username']})
-    if user and bcrypt.checkpw(data['password'].encode('utf-8'), user['password']):
-        return jsonify(message='Login successful'), 200
-    return jsonify(message='Invalid credentials'), 401
+    data = request.get_json()
+    email = data.get("email")
+    password = data.get("password")
+
+    if not email or not password:
+        return jsonify({"message": "Email and password are required"}), 400
+
+    # Supongo que debes buscar al usuario en la colección 'users' de MongoDB y verificar su contraseña
+    user = users_collection.find_one({'username': email})
+    
+    if user and bcrypt.checkpw(password.encode('utf-8'), user['password']):
+        token = create_access_token(identity=str(user['_id']))
+        return jsonify({"token": token}), 200
+
+    return jsonify({"message": "Invalid credentials"}), 401
 
 # Endpoint para agregar un favorito a un usuario
 @app.route('/add_favorite/<user_id>/<pokemon_id>', methods=['POST'])
+@jwt_required()
 def add_favorite(user_id, pokemon_id):
     # Aquí podrías agregar la lógica para almacenar el favorito en la base de datos
+    # También puedes obtener el usuario actualmente autenticado utilizando get_jwt_identity()
+    current_user_id = get_jwt_identity()
     return jsonify(message='Favorite added successfully'), 200
 
 # Endpoint para eliminar un favorito de un usuario
 @app.route('/remove_favorite/<user_id>/<pokemon_id>', methods=['DELETE'])
+@jwt_required()
 def remove_favorite(user_id, pokemon_id):
     # Aquí podrías agregar la lógica para eliminar el favorito de la base de datos
+    # También puedes obtener el usuario actualmente autenticado utilizando get_jwt_identity()
+    current_user_id = get_jwt_identity()
     return jsonify(message='Favorite removed successfully'), 200
 
 if __name__ == '__main__':
--- src/api/routes.py
+++ src/api/routes.py
@@ -23,11 +23,9 @@ def register():
     last_name = data.get("last_name")
     nickname = data.get("nickname")
     birthday = data.get("birthday")
-    
 
     if not email or not password:
         return jsonify({"message": "Email and password are required"}), 400
-    
 
     existing_user = User.query.filter_by(email=email).first()
     if existing_user:
@@ -40,7 +38,6 @@ def register():
         last_name=last_name,
         nickname=nickname,
         birthday=birthday,
-        
     )
 
     db.session.add(new_user)
@@ -58,34 +55,10 @@ def login():
         return jsonify({"message": "Email and password are required"}), 400
 
     user = User.query.filter_by(email=email).first()
-    if user (user.password == password):
+    if user and (user.password == password):
         token = create_access_token(identity=user.id)
         return jsonify({"token": token}), 200
 
-    if not user :
-        return jsonify({"message": "Invalid credentials"}), 401
-          
-   
-
-@api.route("/userPage", methods=["POST"])
-@jwt_required()
-def validate_token():
-    return jsonify({"message": "Token is valid"}), 200
-
-@api.route("/userPage", methods=["POST"])
-@jwt_required()
-def get_user_info():
-    current_user_id = get_jwt_identity()
-    print(current_user_id)
-
-    user = User.query.filter_by(id= current_user_id).first()
-    if user is None:
-        raise APIException("User not found", status_code=404)
-    print(user)
-
-    return jsonify("User authenticated"), 200
-
-    
 
 
 if __name__ == "__main__":
